ceb4c9c863f1732ee531c92ef511de3ec2c58abc
ZakharSytoi <zakharsytoi@gmail.com>
2023-04-28 20:15:56 +0200

    Task completed

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -2,11 +2,13 @@ package com.epam.demo;
 
 import java.util.List;
 
+import static com.epam.utils.StringUtils.isPositiveNumber;
+
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        for (String i : args) {
-            if (Integer.parseInt(i) < 0) return false;
-        }
-        return true;
+       for (String arg: args) {
+           if (!isPositiveNumber(arg)) return false;
+       }
+       return true;
     }
 }
